3afc2c3bcef527f8e1b2366bd3441ad19eca491b
carmenjoekalda <cjoekalda@gmail.com>
2023-10-06 11:26:59 +0300

    Created form request response with API data

--- weather_express/index.js
+++ weather_express/index.js
@@ -9,6 +9,9 @@ app.set('views', path.join(__dirname, 'views'))
 const key = 'b67e81d202909a8fd55f66677a112aba'
 let city = 'Tartu'
 
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
 app.get('/', function (req, res) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
     .then((response) => {
@@ -23,6 +26,24 @@ app.get('/', function (req, res) {
             city: city,
             temp: temp
         })
+    })
+})
+
+app.post('/', function(req, res){
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)-273.15)
+        res.render('index', {
+            description: description,
+            city: city,
+            temp: temp
+        })  
     })
 })
 
